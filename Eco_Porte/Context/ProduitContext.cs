3a5e87d3fd684d38dc2ced7bb7fdb8111b1bf08d
RamiJammeli <60295241+RamiJammeli@users.noreply.github.com>
2021-10-16 00:20:51 +0100

    create project

---
+++ Eco_Porte/Context/ProduitContext.cs
@@ -0,0 +1,19 @@
+﻿using Eco_Porte.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Eco_Porte.Context
+{
+    public class ProduitContext:DbContext
+    {
+        
+        public ProduitContext() : base("ProduitContextDB")
+        {
+        }
+        public DbSet<Produit> Agences { get; set; }
+        public DbSet<Categorie> Categories { get; set; }
+    }
+}
---
+++ Eco_Porte/Models/Produit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eco_Porte.Models
+{
+    public class Produit
+    {
+        public int Id { get; set; }
+        public String Reference { get; set; }
+        public string Nom { get; set; }
+        public string Description { get; set; }
+        public int? Qte { get; set; }
+        public Double? Prix { get; set; }        
+        public String Matiere { get; set; }
+        public String Image { get; set; }
+        public virtual Categorie categorie { get; set; }
+    }
+}
